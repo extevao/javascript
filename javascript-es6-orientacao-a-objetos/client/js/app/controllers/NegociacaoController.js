d9d94f7f6196da10d8d11a3231aa86525a019590
Estevao <estevaovasques@hotmail.com>
2017-01-14 15:02:37 -0200

    Criando a data da string vinda do input

--- javascript-es6-orientacao-a-objetos/client/js/app/controllers/NegociacaoController.js
+++ javascript-es6-orientacao-a-objetos/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,31 @@
 class NegociacaoController{
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
     adiciona(event){
         event.preventDefault();
-        console.log('Chamei!!');
+        
+        
+        // let data = new Date(this._inputData.value.split('-'));
+        // let data = new Date(this._inputData.value.replace(/-/g, ','));
+        
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map(function(item, indice){
+                return item - indice % 2;
+            }));
+        
+        console.log(data);
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade,
+            this._inputValor);
+        
     }
 }
