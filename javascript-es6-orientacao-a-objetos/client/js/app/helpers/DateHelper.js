c026fdb67fd4fd90c4a6e56de51a33a8f508d1f8
Estevao <estevaovasques@hotmail.com>
2017-01-16 14:45:37 -0200

    Utilizando tempalte strin e fail fast

--- javascript-es6-orientacao-a-objetos/client/js/app/helpers/DateHelper.js
+++ javascript-es6-orientacao-a-objetos/client/js/app/helpers/DateHelper.js
@@ -1,13 +1,16 @@
 class DateHelper {
-    constructor(){
-      throw new Error('Esta classe não pode ser instaciada!');
+    constructor() {
+        throw new Error('Esta classe não pode ser instaciada!');
     }
     static dataParaTexto(data) {
-        return data.getDate() +
-            '/' + (data.getMonth() + 1) +
-            '/' + data.getFullYear();
+
+        return `${data.getDate()}/${data.getMonth() + 1 }/ ${data.getFullYear()}`;
+
     }
     static textoParaData(texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Data inválida, deve estar no formato yyyy-mm-dd');
+
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 
     }
