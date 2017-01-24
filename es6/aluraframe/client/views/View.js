56324108d682bdb57188d1ef27273407c6502490
Estevao <estevaovasques@hotmail.com>
2017-01-24 21:14:14 -0200

    Criado método template na classe View, para que se o desenvolvedor não sobrescrever irá gerar um erro alertando para que sobrescreva

--- es6/aluraframe/client/views/View.js
+++ es6/aluraframe/client/views/View.js
@@ -2,8 +2,11 @@ class View{
   constructor(elemento){
     this._elemento = elemento;
   }
-  update(model){
-    this._elemento.innerHTML = this._template(model);
+  template(){
+    throw new Error('Você deve sobrescrever este método em seu template.');
   }
-  
+  update(model){
+    this._elemento.innerHTML = this.template(model);
+  }
+
 }
