75be151f5194ae96c77feec0d1d97ea4a8e443dc
Estevao <estevaovasques@hotmail.com>
2017-01-25 20:58:45 -0200

    Utilizando arrow function, não precisamos passar o contexto, pois as funcoes flexa possuem this léxicoque não muda de acordo com o contexto da função

--- es6/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ es6/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,20 +1,20 @@
 class ListaNegociacoes {
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
+
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
     get negociacoes() {
         return [].concat(this._negociacoes);
     }
-    esvazia(){
-      this._negociacoes = [];
-      Reflect.apply(this._armadilha, this._contexto, [this]);
+    esvazia() {
+        this._negociacoes = [];
+        this._armadilha(this);
 
     }
 }
