4b468459bbaeb6e95f5462fe902627a5916a9c07
Estevao <estevaovasques@hotmail.com>
2017-02-05 20:18:39 -0200

    Baixado o mongoose, e criado arquivo de configuração para acesso do mongodb

--- contribuicao/server.js
+++ contribuicao/server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
+require('./config/database')('localhost/contribuicao');
 
 http.createServer(app).listen(3000, function(){
   console.log('Servidor iniado na porta 3000');
