825ed81e3e7cec29a3cbb28836dd56464978a862
Estevao <estevaovasques@hotmail.com>
2017-02-11 19:52:38 -0200

    gitignore, package.json e gulpfile

---
+++ proxy/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+
+gulp.task('default', ['server'], function() {});
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'public'
+        }
+    });
+    gulp.watch('public/**/*').on('change', browserSync.reload);
+});
