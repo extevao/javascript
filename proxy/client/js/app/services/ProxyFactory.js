83f0585600ee07c9a01a9be334433db941a260b7
Estevao <estevaovasques@hotmail.com>
2017-02-12 15:34:40 -0200

    Criado funcao para interceptacao de ações que setam valores no meu objeto encapsulado pelo proxy

--- proxy/client/js/app/services/ProxyFactory.js
+++ proxy/client/js/app/services/ProxyFactory.js
@@ -1,22 +1,29 @@
-class ProxyFactory{
-  static create(objeto, props, acao){
-    return new Proxy( objeto, {
-        get: function(target, prop, receiver) {
+class ProxyFactory {
+    static create(objeto, props, acao) {
+        return new Proxy(objeto, {
+            get: function(target, prop, receiver) {
 
-            if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
-                return function() {
-                    console.log(`Método chamado: ${prop}`);
-                    Reflect.apply(target[prop], target, arguments);
-                    return acao(target);
+                if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
+                    return function() {
+                        console.log(`Método chamado: ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    }
                 }
+                // só executa se não for função
+                return Reflect.get(target, prop, receiver);
+            },
+            set(target, prop, value, receiver) {
+                if (props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+                return Reflect.set(target, prop, value, receiver);
             }
-            // só executa se não for função
-            return Reflect.get(target, prop, receiver);
-        }
-    });
-  }
+        });
+    }
 
-  static _ehFuncao(funcao){
-    return typeof(funcao) == typeof(Function);
-  }
+    static _ehFuncao(funcao) {
+        return typeof(funcao) == typeof(Function);
+    }
 }
