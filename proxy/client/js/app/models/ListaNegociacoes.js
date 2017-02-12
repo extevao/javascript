280c4cc4951cdb880ac646347cc5f184d2fb4732
Estevao <estevaovasques@hotmail.com>
2017-02-12 14:45:47 -0200

    Utilizando o Proxy

--- proxy/client/js/app/models/ListaNegociacoes.js
+++ proxy/client/js/app/models/ListaNegociacoes.js
@@ -1,25 +1,23 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
-        
+    constructor() {
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        
+
+
     }
 
     get negociacoes() {
-        
+
         return [].concat(this._negociacoes);
     }
 
     esvazia() {
-        
+
         this._negociacoes = [];
         this._armadilha(this);
     }
