c31f205143c43d53788085a0a1a0b292fc7f9923
Estevao <estevaovasques@hotmail.com>
2017-02-13 22:04:49 -0200

    JS na munheca

--- IMC/calcula-imc.js
+++ IMC/calcula-imc.js
@@ -1,11 +1,28 @@
-//calcula-imc.js
-var peso = 100;
-var altura = 0;
+var pacientes = [];
+var paciente = {};
+var elementos = document.getElementsByClassName('paciente');
 
-if(altura != 0){
-    var imc = peso / (altura * altura);
-    console.log(imc);
-} else{
-    console.log("Não posso executar uma divisão por 0!");
+for (var i = 0; i < elementos.length; i++) {
+    paciente.nome =elementos[i].querySelector('.info-nome').textContent;
+    paciente.peso =elementos[i].querySelector('.info-peso').textContent;
+    paciente.altura =elementos[i].querySelector('.info-altura').textContent;
+
+    pacientes.push(paciente);
+
 }
+console.log(pacientes);
 
+
+
+function functionName() {
+    var pessoa = {
+        nome: 'Pedro Silva',
+        idade: 12,
+        telfone: '9999-9999'
+    };
+
+    for (var propriedade in pessoa) {
+        console.log(propriedade + " " + pessoa[propriedade]);
+    }
+
+}
